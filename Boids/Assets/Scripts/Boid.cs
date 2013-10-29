a5d35626d1e48f3b10da0705f8b011214122308f
Matias Snellingen <matias@snellingen.com>
2013-10-29 14:49:12 +0100

    added random speed for each fly

--- Boids/Assets/Scripts/Boid.cs
+++ Boids/Assets/Scripts/Boid.cs
@@ -14,6 +14,7 @@ public class Boid : MonoBehaviour
     public int repelRadius = 4; 
     public int flockForce = 10;
     public int drag = 5;
+    public float speedDif = 2f; 
     public Vector3 direction;
     private Vector3 clusterPostion;
     private ClusterInfo clusterInfo;
@@ -26,6 +27,7 @@ public class Boid : MonoBehaviour
     {
         direction = Vector3.zero;
         clusterInfo = transform.parent.GetComponent<ClusterInfo>();
+        speedDif = Random.Range(1f, speedDif+1f);
     }
 
     void Update()
@@ -46,14 +48,14 @@ public class Boid : MonoBehaviour
             {
                 closest = hitCollider;
                 if(hitCollider.tag == "boid")
-                    rigidbody.AddForce((transform.position - hitCollider.transform.position).normalized * repelForce/2);
-                else rigidbody.AddForce((transform.position - hitCollider.transform.position).normalized * repelForce * 5);
+                    rigidbody.AddForce((transform.position - hitCollider.transform.position).normalized * (repelForce/2) * speedDif);
+                else rigidbody.AddForce((transform.position - hitCollider.transform.position).normalized * repelForce * 5 * speedDif);
             }
-            rigidbody.AddForce((transform.position - closest.transform.position).normalized * repelForce);
+            rigidbody.AddForce((transform.position - closest.transform.position).normalized * repelForce * speedDif);
         }
 
-        rigidbody.AddForce(clusterInfo.clusterDirection.normalized * directionForce);
-        rigidbody.AddForce((clusterInfo.clusterCenter - transform.position).normalized * flockForce);
+        rigidbody.AddForce(clusterInfo.clusterDirection.normalized * directionForce * speedDif);
+        rigidbody.AddForce((clusterInfo.clusterCenter - transform.position).normalized * flockForce * speedDif);
         direction = rigidbody.velocity;
         if (direction != Vector3.zero)
             transform.rotation = Quaternion.LookRotation(direction);
